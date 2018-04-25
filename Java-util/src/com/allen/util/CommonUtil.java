28c845d766c2846a576e5ac428a7ac279c846c1b
ligenfeng <ligenfeng@chinasofti.com>
2018-04-25 21:28:05 +0800

    update CommonUtil

--- Java-util/src/com/allen/util/CommonUtil.java
+++ Java-util/src/com/allen/util/CommonUtil.java
@@ -3,8 +3,11 @@
  */
 package com.allen.util;
 
+import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
 /**
@@ -13,29 +16,36 @@ import java.util.TimeZone;
  */
 public class CommonUtil {
 
+	/** define constant:SEPARATOR*/
+	private static final String SEPARATOR = "/";
 	/**
-	 * Format java.util.Date to "GMT+8" time
+	 * Get now date in GMT+8
 	 */
-	private static void dateFormat(){
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeZone(TimeZone.getTimeZone("GMT+8"));
-        String year = String.valueOf(calendar.get(Calendar.YEAR));
-        String month = String.valueOf(calendar.get(Calendar.MONTH) + 1);
-        String day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
-        System.out.println(year+month+day);
-    }
-	
-	/**
-	 * Format money 
-	 */
-	private static void moneyFormat(){
-        String s = "12345566";
-        DecimalFormat format=new DecimalFormat("###,###.##￥");
-        String str = format.format(Double.parseDouble(s));
-        System.out.println(str);
-    }
-	
-	
+	public static String getDate() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+		String year = String.valueOf(calendar.get(Calendar.YEAR));
+		String month = String.valueOf(calendar.get(Calendar.MONTH) + 1);
+		String day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
+		String date = year+CommonUtil.SEPARATOR+month+CommonUtil.SEPARATOR+day;
+		return date;
+	}
 
+	/**
+	 * Format money
+	 */
+	public static String moneyFormat(Double money) {
+		//$:"###,###.##$"; ¥:"###,###.##￥" ;
+		DecimalFormat format = new DecimalFormat("###,###.##￥");
+		String moneyStr = format.format(money);
+		return moneyStr;
+	}
+
+	public static String dateFormat(Date date) {
+		DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		format.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+		String dateStr = format.format(date);
+		return dateStr;
+	}
 
 }
